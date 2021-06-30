656dc88fbef5bfce873f141cc73033620ac5fd54
Belim <belim@builtbybel.com>
2021-06-30 09:51:09 +0200

    Add files via upload

---
+++ src/RSV/StatusWindow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ReadySunValley
+{
+    public partial class StatusWindow : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+    (
+    int nLeftRect,
+    int nTopRect,
+    int nRightRect,
+    int nBottomRect,
+    int nWidthEllipse,
+    int nHeightEllipse
+    );
+
+        public string StatusText
+        {
+            get
+            {
+                return this.status.Text;
+            }
+            set
+            {
+                this.status.Text = value;
+            }
+        }
+
+        public StatusWindow()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 5, 5));
+        }
+
+        private void StatusWindow_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
