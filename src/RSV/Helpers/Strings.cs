0eb8dc76a748478bed5509a76c290913f4d3a54b
Belim <belim@builtbybel.com>
2021-07-10 21:37:21 +0200

    Add files via upload

--- src/RSV/Helpers/Strings.cs
+++ src/RSV/Helpers/Strings.cs
@@ -2,20 +2,21 @@
 {
     internal class Strings
     {
+        public static readonly string ShareTwitter = "https://twitter.com/intent/tweet?text=" + Locales.Locale.assetTweet;
+
         public static class Uri
         {
             public const string GitRepo = "https://github.com/builtbybel/ReadySunValley";
             public const string GitUpdateRepo = "https://github.com/builtbybel/readysunvalley/releases/tag/";
             public const string GitVersionCheck = "https://raw.githubusercontent.com/builtbybel/readysunvalley/master/appversion.txt";
             public const string GitVersionHint = "https://raw.githubusercontent.com/builtbybel/ReadySunValley/main/changes.txt";
+            public const string GitTestingRelease = "https://github.com/builtbybel/ReadySunValley/issues/20";
 
             public const string CompareMS = "https://github.com/builtbybel/ReadySunValley/blob/main/assets/rsv-microsoft-requirements.png?raw=true";
             public const string CompareUtil = "https://github.com/rcmaehl/WhyNotWin11/releases/download/";
             public const string UtilVersionCheck = "https://raw.githubusercontent.com/builtbybel/ReadySunValley/main/utilversion.txt";
 
             public const string VotePage = "https://www.builtbybel.com/blog/19-apps/41-check-with-the-readysunvalley-app-if-your-device-works-with-windows11-sun-valley-update";
-
-            public const string ShareTwitter = "https://twitter.com/intent/tweet?text=Ready%20for%20%23Windows11/Sun%20Valley%20update?%20Here%20are%20my%20results%20%23ReadySunValley%20%23app";
 
             public const string MSSystemRequirements = "https://aka.ms/WindowsSysReq";
         }
